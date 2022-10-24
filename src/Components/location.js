ea3c50ddd0d2085f64735e937a07b946901b6925
BhavanaSVastrad <bhavanasvastrad@gmail.com>
2022-10-24 09:44:04 +0530

    commit

--- src/Components/location.js
+++ src/Components/location.js
@@ -14,7 +14,10 @@ export default function Location(){
               <i class="fa fa-pinterest fa-lg fa-fw" style={{color:'darkorange'}}></i>
               <i class="fa fa-linkedin fa-lg fa-fw"style={{color:'darkorange'}}></i>
 
- <img src={logo} style={{ height: 200, width: 1400, paddingLeft: 800 }}></img>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.25121098481!2d73.77408491439003!3d18.56270988738444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf559a0d1a05%3A0x71978883d86139e3!2sHaber%20(Elixa%20Technologies%20Private%20Limited)!5e0!3m2!1sen!2sin!4v1666344683241!5m2!1sen!2sin" style={{marginLeft:500,height:200,width:700}}></iframe>
+                  
+     
+
         </div>
        
     )
